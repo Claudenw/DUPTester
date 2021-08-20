874925d39351c0d5738671af5622af1c1d996010
Zhuqi <zhuqijinca@gmail.com>
2021-08-20 12:37:45 -0400

    add set up script

---
+++ DUPTester-setup.sh
@@ -0,0 +1,30 @@
+script="$0"
+CURR_DIR="$(dirname $script)"
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git-lfs
+git lfs migrate import --include="*.tar.gz"
+git add .gitattributes 
+sudo apt-get update
+sudo apt-get install docker-io openjdk-8-jdk python3-docker
+sudo chmod 666 /var/run/docker.sock
+sudo apt-get install ant maven
+
+APP_DIR="$CURR_DIR/applications"
+SYS_DIR="$CURR_DIR/systems"
+mkdir $APP_DIR
+cd $APP_DIR
+
+git clone https://github.com/apache/cassandra
+git clone https://github.com/apache/hbase
+git clone https://github.com/apache/hive
+git clone https://github.com/apache/kafka
+git clone https://github.com/apache/zookeeper
+
+cd ..
+echo $APP_DIR/cassandra > $SYS_DIR/cass/cassandra.srclocation
+echo $APP_DIR/hbase > $SYS_DIR/habse/hbase.srclocation
+echo $APP_DIR/hive > $SYS_DIR/hive/hive.srclocation
+echo $APP_DIR/kafka > $SYS_DIR/kafka/kafka.srclocation
+echo $APP_DIR/zookeeper > $SYS_DIR/zk/zk.srclocation
+
